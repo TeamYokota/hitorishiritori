f5e7621234f9ec9ce694376de381d6c2314aebc4
tyudai <yudai931611@gmail.com>
2015-06-26 12:17:20 +0900

    文字の判定については網羅してると思います。テスト済みです

--- TextCheck/app/src/main/java/com/example/siritori/textcheck/TextCheck.java
+++ TextCheck/app/src/main/java/com/example/siritori/textcheck/TextCheck.java
@@ -31,23 +31,27 @@ public class TextCheck extends Activity implements OnClickListener {
                 new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
     }
 
-    public void onClick(View v)  {
+    public void onClick(View v) {
 
-        String str  = "^[ぁ-ゞ]+$";
+        String str = "^[ぁ-ゞ]+$";
         String str1 = "^{1,13}$";
         String stop = "ちゅうだん";
-        String end  = "ん";
-        String text  = "しりとり";
+        String end = "ん";
+        String text = "しりとり";
         String text2 = "しんぶんし";
         String text3 = "よっしー";
         String nagai = "あああああいいいいいううううう";
         String haihun = "ー";
+        String moji1 = "しりとり";
+        String moji2 = "りんご";
+        String moji3 = "らんらんる";
 
         button.setOnClickListener(this);
 
-        for (int x = 0; x < 3; x++) {
+        for (int x = 0; x < 1; x++) {
 
-            if("ちゅうだん".equals(stop)) {
+            //中断処理
+            if ("ちゅうだん".equals(stop)) {
                 System.out.println("ちゅうだんするため中断処理へ移行する");
                 Log.d("tag", "中断処理へ飛ばす");
             } else {
@@ -55,7 +59,8 @@ public class TextCheck extends Activity implements OnClickListener {
                 Log.d("tag", "次へ");
             }
 
-            if(nagai.matches(str1)) {
+            //13文字判定
+            if (nagai.matches(str1)) {
                 System.out.println("13文字以内");
                 Log.d("tag", "13文字以内");
                 //入力文字数が適正
@@ -66,13 +71,22 @@ public class TextCheck extends Activity implements OnClickListener {
                 //break;
             }
 
-            //////////////////////////////////
 
-            //このタイミングで文頭と語尾の判定
+            //語頭語尾一致確認
+            if (moji1.endsWith(moji2.substring(0, 1))) {
 
-            //////////////////////////////////
+                System.out.println("語頭語尾一致");
+                Log.d("tag", "OK");
 
-            if("ぴーたーぱん".endsWith(end)) {
+            } else {
+
+                System.out.println("語頭語尾一致");
+                Log.d("tag", "NO");
+
+            }
+
+            //”ん”の判定
+            if ("ぴーたーぱん".endsWith(end)) {
                 System.out.println("語尾が「ん」です");
                 Log.d("tag", "勝敗処理へ");
             } else {
@@ -80,17 +94,22 @@ public class TextCheck extends Activity implements OnClickListener {
                 Log.d("tag", "続く");
             }
 
-            if(text3.endsWith(haihun)) {
+            //ハイフンの除去
+            if (text3.endsWith(haihun)) {
                 System.out.println("語尾はハイフン");
                 Log.d("tag", "ー");
+                int n = text3.length();
+                text3 = text3.substring(0,n-1);
+                System.out.println(text3);
                 //この場合はーを排除し次に進みます
-           } else {
+            } else {
                 System.out.println("後方一致ではありません");
                 Log.d("tag", "続く");
                 //特に処理せず次へ
             }
 
-            if(text.equals(str)) {
+            //特殊文字などの排除
+            if (text.equals(str)) {
                 System.out.println("日本語");
                 Log.d("tag", "全角日本語");
                 //入力文字が日本語の場合
@@ -100,7 +119,8 @@ public class TextCheck extends Activity implements OnClickListener {
                 //数字などが含まれる場合
             }
 
-            if(text.equals(str)) { //重複処理
+            //重複確認
+            if (text.equals(str)) {
                 //DBへアクセスし値を取得し右辺へ代入
                 //左辺へは入力された文字を代入
                 System.out.println("一度使われた文字です");
@@ -110,26 +130,6 @@ public class TextCheck extends Activity implements OnClickListener {
                 Log.d("tag", "次へ");
             }
 
-            ///////////////////
-
-            //濁点半濁点を＝にする処理はここ
-
-            //////////////////
-
-
-            //////////////////
-
-            //文字の画面＆音声出力はここで行う
-
-            //////////////////
-
-
         }
-
-
-
-
-
-
     }
 }
